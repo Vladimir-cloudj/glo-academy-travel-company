9cf919f43cc896b31bf2f5e15df6f83122cc0b8c
Vladimir-cloudj <good-speedF1@yandex.ru>
2026-07-29 17:01:12 +0400

    feat: start config

---
+++ src/views/ToursView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper mt-5">
+    <div class="container">
+      <h1>Страница туров</h1>
+      <p>Здесь будет список туров.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToursView'
+}
+</script>
